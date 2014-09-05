b99e4dab3ac75db1ecdc360483c1683b1b9aefbd
Marco Bartkowiak <marco@bartkmq.de>
2014-09-05 22:16:54 +0200

    add hxCollision

---
+++ Libs/hxcollision/shapes/Polygon.hx
@@ -0,0 +1,155 @@
+package hxcollision.shapes;
+import hxcollision.data.CollisionData;
+import hxcollision.shapes.Ray;
+
+import hxcollision.math.Vector;
+import hxcollision.data.RayData;
+import hxcollision.shapes.Shape;
+
+/** A polygon collision shape */
+class Polygon extends Shape
+{
+	
+	/** The transformed (rotated/scale) vertices cache */
+    public var transformedVertices ( get, never ) : Array<Vector>;
+	/** The vertices of this shape */
+    public var vertices ( get, never ) : Array<Vector>;
+
+    var _transformedVertices : Array<Vector>;
+    var _vertices : Array<Vector>;
+	
+	/** Create a new polygon with a given set of vertices at position x,y. */
+    public function new( x:Float, y:Float, vertices:Array<Vector> ) {
+
+        super( x,y );
+        
+        name = vertices.length + 'polygon';
+		
+        _transformedVertices = new Array<Vector>();
+        _vertices = vertices;
+    
+    } //new
+	
+	override public function test(shape:Shape):CollisionData 
+	{
+		return shape.testPolygon(this, true);
+	}
+	
+	override public function testCircle(circle:Circle, flip:Bool = false):CollisionData 
+	{
+		return Collision2D.testCircleVsPolygon( circle, this, flip );
+	}
+	
+	override public function testPolygon(polygon:Polygon, flip:Bool = false):CollisionData 
+	{
+		return Collision2D.testPolygons( this, polygon, flip );
+	}
+	
+	override public function testRay(ray:Ray):RayData 
+	{
+		return Collision2D.rayPolygon(ray, this);
+	}
+        
+        /** Destroy this polygon and clean up. */
+    override public function destroy() : Void {
+
+        var _count : Int = _vertices.length;
+        for(i in 0 ... _count) {
+            _vertices[i] = null;
+        }
+		
+        _transformedVertices = null;
+        _vertices = null;
+        super.destroy();
+
+    } //destroy    
+
+//.transformedVertices
+
+    function get_transformedVertices() : Array<Vector> {
+
+        if(!_transformed) {
+            _transformedVertices = new Array<Vector>();
+            _transformed = true;
+
+            var _count : Int = _vertices.length;
+
+            for(i in 0..._count) {
+                _transformedVertices.push( _vertices[i].clone().transform( _transformMatrix ) );
+            }
+        }
+
+        return _transformedVertices;
+    }
+
+//.vertices 
+
+    function get_vertices() : Array<Vector> {
+        return _vertices;
+    }
+
+        /** Helper to create an Ngon at x,y with given number of sides, and radius.
+            A default radius of 100 if unspecified. Returns a ready made `Polygon` collision `Shape` */    
+    public static function create( x:Float, y:Float, sides:Int, radius:Float=100):Polygon {
+
+        if(sides < 3) {
+            throw 'Polygon - Needs at least 3 sides';
+        }
+
+        var rotation:Float = (Math.PI * 2) / sides;
+        var angle:Float;
+        var vector:Vector;
+        var vertices:Array<Vector> = new Array<Vector>();
+
+        for(i in 0 ... sides) {
+            angle = (i * rotation) + ((Math.PI - rotation) * 0.5);
+            vector = new Vector();
+            vector.x = Math.cos(angle) * radius;
+            vector.y = Math.sin(angle) * radius;
+            vertices.push(vector);
+        }
+        
+        return new Polygon(x,y,vertices);
+
+    } //create
+
+        /** Helper generate a rectangle at x,y with a given width/height and centered state.
+            Centered by default. Returns a ready made `Polygon` collision `Shape` */    
+    public static function rectangle(x:Float, y:Float, width:Float, height:Float, centered:Bool = true):Polygon {
+        
+        var vertices:Array<Vector> = new Array<Vector>();
+
+        if(centered) {
+
+            vertices.push( new Vector( -width / 2, -height / 2) );
+            vertices.push( new Vector(  width / 2, -height / 2) );
+            vertices.push( new Vector(  width / 2,  height / 2) );
+            vertices.push( new Vector( -width / 2,  height / 2) );
+
+        } else {
+
+            vertices.push( new Vector( 0, 0 ) );
+            vertices.push( new Vector( width, 0 ) );
+            vertices.push( new Vector( width, height) );
+            vertices.push( new Vector( 0, height) );
+
+        }
+
+        return new Polygon(x,y,vertices);
+    
+    } //rectangle
+    
+        /** Helper generate a square at x,y with a given width/height with given centered state.
+            Centered by default. Returns a ready made `Polygon` collision `Shape` */
+    public static function square(x:Float, y:Float, width:Float, centered:Bool = true):Polygon {
+        return rectangle(x, y, width, width, centered);
+    } //square
+
+        /** Helper generate a triangle at x,y with a given radius. 
+            Returns a ready made `Polygon` collision `Shape` */
+    public static function triangle(x:Float, y:Float, radius:Float):Polygon {
+        return create(x, y, 3, radius);
+    } //triangle
+
+} //Polygon
+
---
+++ Libs/hxcollision/shapes/Shape.hx
@@ -0,0 +1,182 @@
+package hxcollision.shapes;
+
+
+import hxcollision.Collision;
+import hxcollision.data.CollisionData;
+import hxcollision.math.Matrix;
+import hxcollision.math.Vector;
+import hxcollision.data.RayData;
+
+/** A base collision class shape */
+class Shape {
+
+
+        /** The state of this shape, if inactive can be ignored in results */
+    public var active : Bool = true;
+        /** The name of this shape, to help in debugging */
+    public var name : String = 'shape';
+        /** A generic data object where you can store anything you want, for later use */
+    public var data : Dynamic;
+        /** A list of tags to use for marking shapes with data for later use, by key/value */
+    public var tags : Map<String, String>;
+        /** The position of this shape */
+    public var position ( get, set ) : Vector;
+        /** The x position of this shape */
+    public var x ( get, set ) : Float;
+        /** The y position of this shape */
+    public var y ( get, set ) : Float;
+        /** The rotation of this shape, in degrees */
+    public var rotation ( get, set ) : Float;
+        /** The scale in the x direction of this shape */
+    public var scaleX ( get, set ) : Float;
+        /** The scale in the y direction of this shape */
+    public var scaleY ( get, set ) : Float;
+
+    var _position : Vector;
+    var _rotation : Float = 0;
+    var _rotation_radians : Float = 0;
+    var _scale : Vector;
+
+    var _scaleX : Float = 1;
+    var _scaleY : Float = 1;
+
+    var _transformed : Bool = false;
+    var _transformMatrix : Matrix;
+
+
+//Public API
+
+
+	/** Create a new shape at give position x,y */
+    public function new( _x:Float, _y:Float )
+	{
+        tags = new Map();
+
+        _position = new Vector(_x,_y);
+        _scale = new Vector(1,1);
+        _rotation = 0;
+
+        _scaleX = 1;
+        _scaleY = 1;
+
+        _transformMatrix = new Matrix();
+        _transformMatrix.makeTranslation( _position.x, _position.y );
+    } //new
+
+	public function test( shape:Shape ):CollisionData
+	{
+		return null;
+	}
+
+	public function testCircle( circle:Circle, flip:Bool = false ):CollisionData
+	{
+		return null;
+	}
+
+	public function testPolygon( polygon:Polygon, flip:Bool = false ):CollisionData
+	{
+		return null;
+	}
+
+	public function testRay( ray:Ray ):RayData
+	{
+		return null;
+	}
+
+	/** clean up and destroy this shape */
+	public function destroy():Void {
+        _position = null;
+        _scale = null;
+        _transformMatrix = null;
+    } //destroy
+
+//Getters/Setters
+
+    function refresh_transform() {
+
+        _transformMatrix.rotate( _rotation_radians );
+
+        _transformMatrix.compose( _position, _rotation, _scale );
+        _transformed = false;
+
+    }
+
+//.position
+
+    function get_position() : Vector {
+        return _position;
+    }
+
+    function set_position( v : Vector ) : Vector {
+        _position = v;
+        refresh_transform();
+        return _position;
+    }
+
+//.x
+
+    function get_x() : Float {
+        return _position.x;
+    }
+
+    function set_x(x : Float) : Float {
+        _position.x = x;
+        refresh_transform();
+        return _position.x;
+    }
+
+//.y
+
+    function get_y() : Float {
+        return _position.y;
+    }
+
+    function set_y(y : Float) : Float {
+        _position.y = y;
+        refresh_transform();
+        return _position.y;
+    }
+
+//.rotation
+
+    function get_rotation() : Float {
+        return _rotation;
+    }
+
+    function set_rotation( v : Float ) : Float {
+
+        _rotation_radians = v * (Math.PI / 180);
+
+        refresh_transform();
+
+        return _rotation = v;
+
+    } //set_rotation
+
+//.scaleX
+
+    function get_scaleX():Float {
+        return _scaleX;
+    }
+
+    function set_scaleX( scale : Float ) : Float {
+        _scaleX = scale;
+        _scale.x = _scaleX;
+        refresh_transform();
+        return _scaleX;
+    }
+
+//.scaleY
+
+    function get_scaleY():Float {
+        return _scaleY;
+    }
+
+    function set_scaleY(scale:Float) : Float {
+        _scaleY = scale;
+        _scale.y = _scaleY;
+        refresh_transform();
+        return _scaleY;
+    }
+
+}
