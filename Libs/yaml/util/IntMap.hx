726baf52c5fcd0b668f08653e4eaf5c281823895
Marco Bartkowiak <marco@bartkmq.de>
2014-09-08 00:15:55 +0200

    add hx-yaml as submodule

--- Libs/yaml/util/IntMap.hx
+++
@@ -1,7 +0,0 @@
-package yaml.util;
-
-#if haxe3
-typedef IntMap<T> = haxe.ds.IntMap<T>;
-#else
-typedef IntMap<T> = IntHash<T>;
-#end
--- Libs/yaml/util/StringMap.hx
+++
@@ -1,7 +0,0 @@
-package yaml.util;
-
-#if haxe3
-typedef StringMap<T> = haxe.ds.StringMap<T>;
-#else
-typedef StringMap<T> = Hash<T>;
-#end
